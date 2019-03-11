b5fe66b27c44aa441d0725594c9de26de83fb929
zdgeier <zdgeier@vt.edu>
2019-03-11 03:12:51 -0400

    float cleanup

--- mobile/app/src/main/java/com/vtblockchain/mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/vtblockchain/mobile/MainActivity.kt
@@ -3,7 +3,6 @@ package com.vtblockchain.mobile
 import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
 import com.memtrip.eos.chain.actions.transaction.TransactionContext
@@ -15,27 +14,12 @@ import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
-import androidx.annotation.NonNull
-import com.google.android.gms.tasks.OnFailureListener
 import android.widget.Toast
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.FusedLocationProviderClient
-import android.Manifest.permission
-import android.Manifest.permission.ACCESS_FINE_LOCATION
 import androidx.core.app.ActivityCompat
-import android.location.LocationManager
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.core.content.ContextCompat
 import android.os.StrictMode
-
-
-
-
-
-
-
 
 class MainActivity : AppCompatActivity() {
     fun checkPermission() {
@@ -46,8 +30,7 @@ class MainActivity : AppCompatActivity() {
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
-        ) {//Can add more as per requirement
-
+        ) {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
@@ -60,19 +43,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
-            checkPermission();
-        }
+        checkPermission()
 
         val button : Button = findViewById(R.id.submit)
         val account : TextInputEditText = findViewById(R.id.account)
         val privateKey : TextInputEditText = findViewById(R.id.privateKey)
-        val location : TextInputEditText = findViewById(R.id.location)
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
-        }
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
         val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -85,14 +63,11 @@ class MainActivity : AppCompatActivity() {
 
             val api = Api(Config.LOCAL_HOST_API_BASE_URL, okHttpClient.build())
 
-            var currentLat : Double = 0.0
-            var currentLong : Double = 0.0
-
             try {
                 mFusedLocationClient.lastLocation.addOnSuccessListener { location ->
                     // GPS location can be null if GPS is switched off
-                    currentLat = location.latitude
-                    currentLong = location.longitude
+                    val currentLat = location.latitude
+                    val currentLong = location.longitude
                     Toast.makeText(
                         this@MainActivity,
                         "lat " + currentLat + "\nlong " + currentLong,
