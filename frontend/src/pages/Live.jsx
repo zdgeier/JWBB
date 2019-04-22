c59181a569d179462cefd595c9468e5f9aa710f9
nmangaokar <neal1998@vt.edu>
2019-04-22 17:45:38 -0400

    new about page layout

--- frontend/src/pages/Live.jsx
+++ frontend/src/pages/Live.jsx
@@ -13,9 +13,14 @@ import Card from "@material-ui/core/Card/Card";
 
 import {JsonRpc} from "eosjs";
 import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
+import * as ReactDOM from "react-dom";
+import FormControl from "@material-ui/core/FormControl/FormControl";
+import InputLabel from "@material-ui/core/InputLabel/InputLabel";
+import Select from "@material-ui/core/Select/Select";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import OutlinedInput from "@material-ui/core/OutlinedInput/OutlinedInput";
 
-//const endpoint = "http://localhost:8888";
-const endpoint = "http://172.29.5.63:8888";
+const endpoint = "http://localhost:8888";
 
 const styles = theme => ({
     root: {
@@ -31,8 +36,23 @@ const styles = theme => ({
         top: "50%",
         left: "50%",
         textAlign: 'center',
+    },
+    chooseClassRoot: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60%"
+    },
+    selectFormControl: {
+        margin: "auto",
+        width: "45%",
+        minWidth: 520,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2
     }
 });
+
 
 class LiveAttendanceView extends Component {
     constructor(props) {
@@ -56,21 +76,25 @@ class LiveAttendanceView extends Component {
     }
 
     refreshLiveAttendance() {
-        this.liveAttendancePollingId = setInterval(this.getAttendance.bind(this), 1000);
+        this.liveAttendancePollingId = setInterval(() => {
+            this.getAttendance.bind(this)(this.props.crn)
+        }, 1000);
     }
 
-    getAttendance() {
-        //console.log(this);
+    getAttendance(crn) {
         const rpc = new JsonRpc(endpoint);
         rpc.get_table_rows({
             "json": true,
-            "code": "attendit",   // contract who owns the table
-            "scope": "attendit",  // scope of the table
-            "table": "attendance",    // name of the table as specified by the contract abi
+            "code": "lokchain",   // contract who owns the table
+            "scope": "lokchain",  // scope of the table
+            "table": "attendance",    // name of the table as specified by the contract abi,
             "limit": 100,
-        }).then(result => this.setState({
-            attendees: result.rows
-        }));
+        }).then(result => {
+            console.log(result);
+            this.setState({
+                attendees: result.rows
+            })
+        });
     }
 
     componentWillUnmount() {
@@ -92,15 +116,10 @@ class LiveAttendanceView extends Component {
                 </div>
             );
         }
-        let heatmapData = [];
-        for (let record in this.state) {
-            heatmapData.push({
-                x: record.xval,
-                y: record.yval,
-                data: record.timestamp,
-            })
-        }
-        let attendeeCards = this.state.attendees.map((record) => {
+        let attendees = this.state.attendees.filter((record) => {
+            return record.crn === this.props.crn;
+        });
+        let attendeeCards = attendees.map((record) => {
             return (
                 <Fade in={true} timeout={500}>
                     <Grid item xs={4}>
@@ -143,4 +162,88 @@ class LiveAttendanceView extends Component {
     }
 }
 
-export default withStyles(styles)(LiveAttendanceView);
+class SelectLiveClass extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            crn: "",
+            name: "hai",
+            labelWidth: 0
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth
+        });
+
+    }
+
+    handleChange = event => {
+        this.setState({[event.target.name]: event.target.value});
+    };
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <div className={classes.chooseClassRoot}>
+                <form autoComplete="off">
+                    <FormControl className={classes.selectFormControl}>
+                        <InputLabel ref={ref => {
+                            this.InputLabelRef = ref;
+                        }} htmlFor="crn">Select the CRN of the class for which you wish to view live
+                            attendance</InputLabel>
+                        <Select
+                            value={this.state.crn}
+                            onChange={this.handleChange}
+                            inputProps={{
+                                name: "crn",
+                                id: "crn"
+                            }}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={12345}>12345</MenuItem>
+                        </Select>
+                    </FormControl>
+                </form>
+                <Button style={{marginTop: 15, marginLeft: 20}} variant={"outlined"}
+                        onClick={() => {
+                            this.props.setClass(this.state.crn);
+                        }}>Go Live</Button>
+            </div>
+        );
+    }
+}
+
+let LiveAttendanceViewStyled = withStyles(styles)(LiveAttendanceView);
+let SelectLiveClassStyled = withStyles(styles)(SelectLiveClass);
+
+class Live extends React.Component {
+    constructor(props) {
+        super(props);
+        this.setClass = this.setClass.bind(this);
+        this.state = {
+            classCrn: -1,
+        }
+    }
+
+    setClass(crn) {
+        this.setState((prevState => {
+            let newState = Object.assign(prevState);
+            newState.classCrn = crn;
+            return newState;
+        }));
+    }
+
+    render() {
+        let {classCrn} = this.state;
+        return (
+            classCrn === -1 ? <SelectLiveClassStyled setClass={this.setClass}/> :
+                <LiveAttendanceViewStyled crn={parseInt(this.state.classCrn)}/>
+        );
+    }
+}
+
+export default Live;
