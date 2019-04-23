e10384c68ec19d4b72721d8e5ccb6dde04e749bd
Nicolas Hardy <nickhrdy@vt.edu>
2019-04-23 14:33:51 -0400

    Make permision setting automatic; remove default class auto creation"

--- eosio_docker/scripts/professor_setup.sh
+++ eosio_docker/scripts/professor_setup.sh
@@ -5,8 +5,8 @@ cleos wallet import -n eosiomain --private-key 5KCi3BQBXzk7maHmzeaiHRjUCmcT74Qe2
 cleos set account permission kirkkcameron professor EOS7fuG2YWbHcaowde6fuovmFRjgqThLMsCTtupDFMTUN9XYafHBZ
 cleos set action permission kirkkcameron attendit record professor
 
-echo "--populate tables--"
-cleos push action attendit populate '["kirkkcameron"]' -p kirkkcameron@active
+#echo "--populate tables--"
+#cleos push action attendit populate '["kirkkcameron"]' -p kirkkcameron@active
 
 echo to test: \n cleos push action attendit record '["kirkkcameron","nicolashardy","100","54","100"]' -p kirkkcameron@professor
 
